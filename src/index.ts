3a7636afe392622bbea5b9f5abb82efab806c43f
leozbroca <leo.brokka@gmail.com>
2023-09-19 18:58:00 -0300

    debbug 7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import app from "./app";
 import EmailController from "./controller/EmailController"
 
-app.post("/send", EmailController.EmailSend)
-app.get("/test", function(req, res) {
-    res.status(200).send({message: "ZE DA MANGA"});
-})
+app.get("/send", EmailController.EmailSend)
